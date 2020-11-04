34141efa4def72b66f0cafc4320d36015330a775
veena-02 <vc7373@srmist.edu.in>
2020-11-04 21:11:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1=Math.floor(6*Math.random())+1;
+var randomNumber2=Math.floor(6*Math.random())+1;
+document.querySelector(".img1").setAttribute("src",("images/dice"+randomNumber1+".png"));
+document.querySelector(".img2").setAttribute("src",("images/dice"+randomNumber2+".png"));
+console.log(randomNumber1);
+console.log(randomNumber2);
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent="Player 1 Wins";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").textContent="Player 2 Wins";
+}
+else{
+
+    document.querySelector("h1").textContent="It's A Draw";
+}
